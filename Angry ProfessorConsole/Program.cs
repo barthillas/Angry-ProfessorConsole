975cf699dfb4120daeb0fb5c07d0a2ec27a34255
Ümit Veysel Okçu <barthillas@hotmail.com>
2018-04-10 01:24:47 +0300

    Add project files.

---
+++ Angry ProfessorConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Angry_ProfessorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
